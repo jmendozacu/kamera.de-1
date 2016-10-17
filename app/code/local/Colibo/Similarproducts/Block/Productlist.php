afa8ff866f797bc4e3e932e6372179a73786dbbd
Roman Baykov <baykovroman88@gmail.com>
2016-10-17 13:48:37 +0300

    KAM-72 - refactored

--- app/code/local/Colibo/Similarproducts/Block/Productlist.php
+++ app/code/local/Colibo/Similarproducts/Block/Productlist.php
@@ -5,17 +5,14 @@ class Colibo_Similarproducts_Block_Productlist extends Mage_Catalog_Block_Produc
     public function getSimilarProducts()
     {
         $product = $this->getProduct();
-        $amazonAttributes = json_decode($product->getAmazonAttributes());
-        $amazonProducts = (isset($amazonAttributes->SimilarProducts->SimilarProduct)) ? $amazonAttributes->SimilarProducts->SimilarProduct : [];
+        $amazonAttributes = json_decode($product->getAmazonAttributes(),true);
+        $amazonProducts = (isset($amazonAttributes['SimilarProducts']['SimilarProduct'])) ? $amazonAttributes['SimilarProducts']['SimilarProduct'] : [];
 
-        $productsSku = [];
-        foreach ($amazonProducts as $amazonProduct) {
-            $productsSku[] = $amazonProduct->ASIN;
-        }
+        $productSkus = array_column($amazonProducts,'ASIN');
 
         $productCollection = Mage::getModel('catalog/product')->getCollection()
             ->addAttributeToSelect('name')
-            ->addAttributeToFilter('sku', array('in' => $productsSku))
+            ->addAttributeToFilter('sku', array('in' => $productSkus))
             ->setPageSize(5)
             ->setCurPage(1);
 
