2d736cefbe60f51bb9afc744d54d8232d80d1d66
Roman Baykov <baykovroman88@gmail.com>
2016-10-17 12:59:56 +0300

    KAM-74 - added quickshop, refactored.

--- app/code/local/Colibo/Similarproducts/Block/Productlist.php
+++ app/code/local/Colibo/Similarproducts/Block/Productlist.php
@@ -5,21 +5,20 @@ class Colibo_Similarproducts_Block_Productlist extends Mage_Catalog_Block_Produc
     public function getSimilarProducts()
     {
         $product = $this->getProduct();
-        $amazonAttributes = json_decode(Mage::getResourceModel('catalog/product')->getAttributeRawValue($product->getId(), 'amazon_attributes'));
-        $amazonProducts = $amazonAttributes->SimilarProducts->SimilarProduct;
+        $amazonAttributes = json_decode($product->getAmazonAttributes());
+        $amazonProducts = (isset($amazonAttributes->SimilarProducts->SimilarProduct)) ? $amazonAttributes->SimilarProducts->SimilarProduct : [];
 
-        $products = [];
-
-        while(count($products) < 5 && ($amazonProduct = array_pop($amazonProducts)) !== null)
-        {
-
-            $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $amazonProduct->ASIN);
-            if($product !== false)
-            {
-                $products[] = $product;
-            }
+        $productsSku = [];
+        foreach ($amazonProducts as $amazonProduct) {
+            $productsSku[] = $amazonProduct->ASIN;
         }
 
-        return $products;
+        $productCollection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('name')
+            ->addAttributeToFilter('sku', array('in' => $productsSku))
+            ->setPageSize(5)
+            ->setCurPage(1);
+
+        return $productCollection;
     }
 }
