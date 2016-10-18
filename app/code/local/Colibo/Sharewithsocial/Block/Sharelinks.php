7ef52cf403f12ee37d25f10d0fb8746956162294
Roman Baykov <baykovroman88@gmail.com>
2016-10-18 18:52:13 +0300

    KAM-75 - refactored

--- app/code/local/Colibo/Sharewithsocial/Block/Sharelinks.php
+++ app/code/local/Colibo/Sharewithsocial/Block/Sharelinks.php
@@ -2,8 +2,4 @@
 
 class Colibo_Sharewithsocial_Block_Sharelinks extends Mage_Core_Block_Template
 {
-    public function getUrl()
-    {
-        return Mage::helper('core/url')->getCurrentUrl();
-    }
 }
