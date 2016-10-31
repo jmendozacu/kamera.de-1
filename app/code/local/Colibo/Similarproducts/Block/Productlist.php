45f4ce82cd2ed93102a7aca7bd265abfd365795e
Roman Baykov <baykovroman88@gmail.com>
2016-10-31 14:14:06 +0300

    KAM-87 - fixed bugs with images in similar products and lastviewed modules

--- app/code/local/Colibo/Similarproducts/Block/Productlist.php
+++ app/code/local/Colibo/Similarproducts/Block/Productlist.php
@@ -11,7 +11,7 @@ class Colibo_Similarproducts_Block_Productlist extends Mage_Catalog_Block_Produc
         $productSkus = array_column($amazonProducts,'ASIN');
 
         $productCollection = Mage::getModel('catalog/product')->getCollection()
-            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('*')
             ->addAttributeToFilter('sku', array('in' => $productSkus))
             ->setPageSize(5)
             ->setCurPage(1);
