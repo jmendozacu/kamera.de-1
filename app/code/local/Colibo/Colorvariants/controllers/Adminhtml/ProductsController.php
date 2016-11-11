6981a061345ac0bdfef7a218cc72b19dafd37f1e
Roman Baykov <baykovroman88@gmail.com>
2016-11-11 17:50:31 +0300

    KAM-96 - refactored

--- app/code/local/Colibo/Colorvariants/controllers/Adminhtml/ProductsController.php
+++ app/code/local/Colibo/Colorvariants/controllers/Adminhtml/ProductsController.php
@@ -10,16 +10,27 @@ class Colibo_Colorvariants_Adminhtml_ProductsController extends Mage_Adminhtml_C
         set_time_limit(0);
         $collection = Mage::getModel('catalog/product')->getCollection()
 
-            ->addAttributeToSelect('*');
-        foreach($collection as $product)
-        {
-            $amazonAttributes = json_decode($product->getAmazonAttributes(),true);
-            if(isset($amazonAttributes['ParentASIN']))
+            ->addAttributeToSelect('*')
+            ->setPageSize(500);
+
+        $pages = $collection->getLastPageNumber();
+        $currentPage = 1;
+        do {
+            $collection->setCurPage($currentPage);
+            $collection->load();
+
+            foreach($collection as $product)
             {
-                $product->setParentAsin($amazonAttributes['ParentASIN'])->save();
-                echo $product->getId()."<br/>";
+                $amazonAttributes = json_decode($product->getAmazonAttributes(),true);
+                if(isset($amazonAttributes['ParentASIN']))
+                {
+                    $product->setParentAsin($amazonAttributes['ParentASIN'])->save();
+                    echo $product->getId()."<br/>";
+                }
             }
-        }
+            $currentPage++;
+            $collection->clear();
+        } while ($currentPage <= $pages);
     }
 }
 ?>
